1553db6acd3caf0b8ee203c12253b8c8443e351e
Sean van Mulligen <sean@vanmulligen.ca>
2017-04-29 11:35:08 -0700

    Add gentellela theme

--- db/migrate/20170429024706_add_title_description_to_issues.rb
+++
@@ -1,6 +0,0 @@
-class AddTitleDescriptionToIssues < ActiveRecord::Migration[5.0]
-  def change
-    add_column :issues, :title, :string
-    add_column :issues, :description, :text
-  end
-end
--- db/migrate/20170429031415_add_severity_to_issues.rb
+++
@@ -1,5 +0,0 @@
-class AddSeverityToIssues < ActiveRecord::Migration[5.0]
-  def change
-    add_column :issues, :severity, :integer
-  end
-end
