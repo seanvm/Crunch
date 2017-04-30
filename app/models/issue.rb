d81b86a23399c6e7ef30c06793a3e8c89bce93a6
Sean van Mulligen <sean@vanmulligen.ca>
2017-04-30 15:30:14 -0700

    Add state management

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,38 @@
-class Issue < ApplicationRecord
-	def sla_countdown
-		# binding.pry
-		SlaTimeService.call(self.severity,self.created_at)
-	end
+class Issue < ActiveRecord::Base
+  include AASM
+
+  aasm column: "state" do
+    state :pending, initial: true
+    state :in_review
+    state :confirmed
+    state :development
+    state :completed
+    state :unverified
+
+    event :review do
+      transitions to: :in_review
+    end
+
+    event :confirm do
+      transitions to: :confirmed
+    end
+    
+    event :development do
+      transitions to: :development
+    end
+    
+    event :completed do
+      transitions to: :completed
+    end
+    
+    event :unverified do
+      transitions to: :unverified
+    end
+  end
+  
+  validates :title, presence: true
+  
+  def sla_countdown
+    SlaTimeService.call(self.severity,self.created_at)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429031415) do
+ActiveRecord::Schema.define(version: 20170430220752) do
 
   create_table "issues", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "title"
     t.text     "description"
     t.integer  "severity"
+    t.string   "state"
   end
 
 end
