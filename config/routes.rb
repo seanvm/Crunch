c6b19764729f9312ae2f40e92042c9c107c3cfea
Sean van Mulligen <sean@vanmulligen.ca>
2017-04-30 15:30:07 -0700

    Update issue pages to match template

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   resources :issues
   root 'plainpage#index'
+  get ':form' => 'plainpage#form'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
