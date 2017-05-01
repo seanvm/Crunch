ad6a24e1dceb9f79bc3915239206ab122346aab8
Sean van Mulligen <sean@vanmulligen.ca>
2017-04-30 22:42:22 -0700

    Fix spacing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,21 +17,21 @@ module SessionsHelper
   end
 
   # Redirects to stored location (or to the default).
- def redirect_back_or(default)
+  def redirect_back_or(default)
    redirect_to(session[:forwarding_url] || default)
    session.delete(:forwarding_url)
- end
+  end
 
- # Stores the URL trying to be accessed.
- def store_location
+  # Stores the URL trying to be accessed.
+  def store_location
    session[:forwarding_url] = request.url if request.get?
- end
- 
- def logged_in?
-   !current_user.nil?
- end
+  end
 
- def current_user?(user)
+  def logged_in?
+   !current_user.nil?
+  end
+
+  def current_user?(user)
    user == current_user
- end
+  end
 end
